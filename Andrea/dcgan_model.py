61fe7f44841730d06e31f8a8dfd1ccfc6d535b6e
andreanr <andrea.navarrete126@gmail.com>
2018-12-09 23:05:07 -0500

    add discriminator

--- Andrea/dcgan_model.py
+++ Andrea/dcgan_model.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 
 
-def conv_layer(input_x, in_channel, out_channel, stride, kernel_shape, rand_seed, index=0):
+def conv_layer(input_x, out_channel, stride=2, kernel_shape=5, rand_seed=2813, index=0):
     """
     :param input_x: The input of the conv layer. Should be a 4D array like (batch_num, img_len, img_len, channel_num)
     :param in_channel: The 4-th demension (channel number) of input matrix. For example, in_channel=3 means the input contains 3 channels.
@@ -11,11 +11,10 @@ def conv_layer(input_x, in_channel, out_channel, stride, kernel_shape, rand_seed
     :param rand_seed: An integer that presents the random seed used to generate the initial parameter value.
     :param index: The index of the layer. It is used for naming only.
     """
-    assert len(input_x.shape) == 4 and input_x.shape[1] == input_x.shape[2] and input_x.shape[3] == in_channel
 
     with tf.variable_scope('conv_layer_%d' % index):
         with tf.name_scope('conv_kernel'):
-            w_shape = [kernel_shape, kernel_shape, in_channel, out_channel]
+            w_shape = [kernel_shape, kernel_shape, input_.get_shape()[-1], out_channel]
             weight = tf.get_variable(name='conv_kernel_%d' % index, shape=w_shape,
                                      initializer=tf.glorot_uniform_initializer(seed=rand_seed))
 
@@ -63,6 +62,40 @@ def conv_transpose_layer(input_x, output_shape, stride=2, kernel_shape=5, rand_s
     return weight, bias, cell_out
 
 
+def fc_layer(input_x, out_size, rand_seed, activation_function=None, index=0):
+    """
+    :param input_x: The input of the FC layer. It should be a flatten vector.
+    :param in_size: The length of input vector.
+    :param out_size: The length of output vector.
+    :param rand_seed: An integer that presents the random seed used to generate the initial parameter value.
+    :param keep_prob: The probability of dropout. Default set by 1.0 (no drop-out applied)
+    :param activation_function: The activation function for the output. Default set to None.
+    :param index: The index of the layer. It is used for naming only.
+
+    """
+    shape = input_x.get_shape().as_list()
+
+    with tf.variable_scope('fc_layer_%d' % index):
+        with tf.name_scope('fc_kernel'):
+            w_shape = [shape[1], out_size]
+            weight = tf.get_variable(name='fc_kernel_%d' % index, shape=w_shape,
+                                     initializer=tf.glorot_uniform_initializer(seed=rand_seed))
+
+        with tf.variable_scope('fc_kernel'):
+            b_shape = [out_size]
+            bias = tf.get_variable(name='fc_bias_%d' % index, shape=b_shape,
+                                   initializer=tf.glorot_uniform_initializer(seed=rand_seed))
+
+        cell_out = tf.add(tf.matmul(input_x, weight), bias)
+        if activation_function is not None:
+            cell_out = activation_function(cell_out)
+
+        tf.summary.histogram('fc_layer/{}/kernel'.format(index), weight)
+        tf.summary.histogram('fc_layer/{}/bias'.format(index), bias)
+
+    return cell_out, weight, bias
+
+
 def norm_layer(input_x, is_training):
     """
     :param input_x: The input that needed for normalization.
@@ -92,17 +125,6 @@ def norm_layer(input_x, is_training):
     return cell_out
 
 
-def linear_layer(input_x, output_size, stddev=0.02, bias_start=0.0):
-    shape = input_x.get_shape().as_list()
-
-    with tf.variable_scope("Linear"):
-        matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
-                                 tf.random_normal_initializer(stddev=stddev))
-        bias = tf.get_variable("bias", [output_size],
-                               initializer=tf.constant_initializer(bias_start))
-
-    return tf.matmul(input_x, matrix) + bias, matrix, bias
-
 
 class DCGAN(object):
     def __init__(self, session, batch_size=64, gf_dim=64, is_training=True):
@@ -114,15 +136,15 @@ class DCGAN(object):
     self.gf_dim = gf_dim
     self.is_training = is_training
 
-    def generator(self, input_x):
+    def generator(self, z):
         """
         """
         with tf.variable_scope("Generator"):
             # Proyect and reshape
-            # Project - Lineal layer
-            self.z, self.h0_w, self.h0_b = linear_layer(input_x, self.gf_dim*8*4*4)
+            # Project - Fully Connected layer
+            self.new_z, self.h0_w, self.h0_b = fc_layer(z, out_size = self.gf_dim*8*4*4, self.seed)
             # Reshape
-            conv_layer_0 = tf.reshape(self.z, [-1, 4, 4, self.gf_dim * 8])
+            conv_layer_0 = tf.reshape(self.new_z, [-1, 4, 4, self.gf_dim * 8])
             # batch normalization
             conv_layer_0 = norm_layer(conv_layer_0, self.is_training)
             # relu
@@ -166,3 +188,49 @@ class DCGAN(object):
             # tanh
             return tf.nn.tanh(conv_layer_4)
 
+    def discriminator(self, image, reuse=False):
+        """
+        """
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+
+        # convolution 0
+        conv_layer_0 = conv_layer(image,
+                                  out_channel=self.df_dim,
+                                  rand_seed=self.seed,
+                                  index=0)
+        # leaky relu
+        conv_layer_0 = tf.nn.leaky_relu(conv_layer_0, alpha=0.2)
+
+        # convolution 1
+        conv_layer_1 = conv_layer(conv_layer_1,
+                                  out_channel=self.df_dim * 2,
+                                  rand_seed=self.seed,
+                                  index=1)
+        # leaky relu
+        conv_layer_1 = tf.nn.leaky_relu(conv_layer_1, alpha=0.2)
+
+        # convolution 2
+        conv_layer_2 = conv_layer(conv_layer_2,
+                                  out_channel=self.df_dim * 4,
+                                  rand_seed=self.seed,
+                                  index=2)
+        # leaky relu
+        conv_layer_2 = tf.nn.leaky_relu(conv_layer_2, alpha=0.2)
+
+        # convolution 3
+        conv_layer_3 = conv_layer(conv_layer_3,
+                                  out_channel=self.df_dim * 8,
+                                  rand_seed=self.seed,
+                                  index=3)
+        # leaky relu
+        conv_layer_3 = tf.nn.leaky_relu(conv_layer_3, alpha=0.2)
+
+        # reshape
+        norm_shape = conv_layer_3.get_shape()
+        img_vector_length = norm_shape[1].value * norm_shape[2].value * norm_shape[3].value
+        flatten = tf.reshape(conv_layer_3, shape=[-1, img_vector_length])
+        # linear
+        fc_layer_4, _, _ = fc_layer(flatten, out_size = 1, self.seed)
+
+        return tf.nn.sigmoid(fc_layer_4), fc_layer_4
